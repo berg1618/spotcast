07b2f488ea8c80eeaabc4605175e079b28c60047
berg1618 <berg@blickdev.com>
2023-06-05 12:00:01 -0300

    criar controls e coverArea

--- screens/Player.js
+++ screens/Player.js
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import { ChevronIcon } from "../components/icons/Chevron";
 import { MoreVertIcon } from "../components/icons/MoreVert";
+import { PlayIcon } from "../components/icons/Play";
 import { LinearGradient } from "expo-linear-gradient";
 
 const Background = ({ children }) => {
@@ -43,12 +44,39 @@ TopBar.SubTitle = styled.Text`
 
 const ScreenArea = styled.View`
   flex: 1;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 32px;
 `;
 
-const CoverArea = styled.View``;
-CoverArea.Image = styled.Image``;
+const CoverArea = styled.View`
+  flex: 4;
+`;
+CoverArea.Image = styled.Image`
+  width: 100%;
+  flex: 1;
+`;
 
-const PlayerArea = styled.View``;
+const PlayerArea = styled.View`
+  flex: 2;
+  background: blue;
+  justify-content: flex-end;
+`;
+
+PlayerArea.Title = styled.Text`
+  color: white;
+  font-size: 20px;
+`;
+
+PlayerArea.Author = styled.Text`
+  color: #bbbbbb;
+  font-size: 14px;
+`;
+
+const Controls = styled.View`
+`;
+
+Controls.Play = styled.Text``;
 
 export default function Player() {
   return (
@@ -71,13 +99,25 @@ export default function Player() {
       <ScreenArea>
         <CoverArea>
           <CoverArea.Image
+            resizeMode="contain"
             source={{
               uri: "https://placehold.it/750x750",
             }}
           />
         </CoverArea>
 
-        <PlayerArea></PlayerArea>
+        <PlayerArea>
+          <PlayerArea.Title>
+            POR QUE ALGUMAS IGREJAS REJEITAM A PSICOLOGIA?
+          </PlayerArea.Title>
+          <PlayerArea.Author>BTPapo</PlayerArea.Author>
+        </PlayerArea>
+
+        <Controls>
+          <Controls.Play>
+            <PlayIcon width={88} height={88} />
+          </Controls.Play>
+        </Controls>
       </ScreenArea>
     </Background>
   );
