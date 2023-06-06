58d61c233de2968ce934e87acab2e8678a93991d
berg1618 <berg@blickdev.com>
2023-06-06 08:56:16 -0300

    criar detalhes nos controles

--- screens/Player.js
+++ screens/Player.js
@@ -3,11 +3,12 @@ import { ChevronIcon } from "../components/icons/Chevron";
 import { MoreVertIcon } from "../components/icons/MoreVert";
 import { PlayIcon } from "../components/icons/Play";
 import { LinearGradient } from "expo-linear-gradient";
+import { Slider } from "react-native";
 
 const Background = ({ children }) => {
   return (
     <LinearGradient
-      colors={["#365796", "#F0AEA3"]}
+      colors={["#365796", "black"]}
       style={{ flex: 1, paddingTop: 50 }}
     >
       {children}
@@ -44,9 +45,7 @@ TopBar.SubTitle = styled.Text`
 
 const ScreenArea = styled.View`
   flex: 1;
-  padding-left: 32px;
-  padding-right: 32px;
-  padding-bottom: 32px;
+  padding: 32px;
 `;
 
 const CoverArea = styled.View`
@@ -59,7 +58,6 @@ CoverArea.Image = styled.Image`
 
 const PlayerArea = styled.View`
   flex: 2;
-  background: blue;
   justify-content: flex-end;
 `;
 
@@ -74,10 +72,29 @@ PlayerArea.Author = styled.Text`
 `;
 
 const Controls = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
 
-Controls.Play = styled.Text``;
+Controls.Play = styled.TouchableOpacity``;
 
+Controls.Slider = styled.View`
+  flex-basis: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+Controls.Slider.CurrentTime = styled.View`
+  color: #bbbbbb;
+`;
+
+Controls.Slider.TotalTime = styled.View`
+  color: #bbbbbb;
+`;
+
+// const AudioSlider = '';
 export default function Player() {
   return (
     <Background>
@@ -114,6 +131,21 @@ export default function Player() {
         </PlayerArea>
 
         <Controls>
+          <Controls.Slider>
+            {/* <AudioSlider
+              thumbTintColor="#FFFFFF"
+              minimumTrackTintColor="#1DD65F"
+              maximumTrackTintColor="#777777"
+              minimumValue={0}
+              maximumValue={100}
+              value={segundos}
+              onValueChange={(value) => {
+                setSegundos(value);
+              }}
+            /> */}
+            <Controls.Slider.CurrentTime>0:00</Controls.Slider.CurrentTime>
+            <Controls.Slider.TotalTime>52:07</Controls.Slider.TotalTime>
+          </Controls.Slider>
           <Controls.Play>
             <PlayIcon width={88} height={88} />
           </Controls.Play>
